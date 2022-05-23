89376c0f1562d290e1d3df45f613c6da09dbd478
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-05-23 18:02:25 +0200

    responsive 1 colomn ok

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('.btn');
+/* const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
 
@@ -8,7 +8,7 @@ btn.addEventListener('click', () => {
         behavior: "smooth"
     })
 
-})
+}) */
 
 
 var TxtType = function (el, toRotate, period) {
@@ -67,3 +67,36 @@ window.onload = function () {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+var target = document.querySelector(".soft-skills");
+function callback(entries, observer) {
+    // The callback will return an array of entries, even if you are only observing a single item
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Show button
+            scrollToTopBtn.classList.add('showBtn')
+        } else {
+            // Hide button
+            scrollToTopBtn.classList.remove('showBtn')
+        }
+    });
+}
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
